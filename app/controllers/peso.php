79b4adf1c34303ab46d90194458306a0a6396109
Alejandrotrox117 <gilalejandro926@gmail.com>
2025-03-30 15:14:10 -0400

    "guardo cambios en peso.php"

--- app/controllers/peso.php
+++ app/controllers/peso.php
@@ -1,32 +1,19 @@
 <?php
-// filepath: app/controllers/peso.php
 
-require_once "app/core/Controllers.php";
-require_once "helpers\helpers.php";
-class Peso extends Controllers
+
+class Peso extends Controllers 
 {
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(); // Llama al constructor de la clase base
     }
 
-    public function app()
-    {
-        // Implementación del método app
-        //echo "Método app ejecutado correctamente.";
-    }
-    public function peso($params = null) {
-        $data['page_id'] = 1;
-        $data["page_title"] = "Pagina principal";
-        $data["tag_page"] = "La pradera de pavia";
-        $data["page_name"] = "Pesaje";
-        
-        // Verifica si hay parámetros
-        if ($params) {
-            echo "Parámetros recibidos: " . $params;
-        }
-    
+    public function peso() {
+        $data['page_title'] = "Gestión de Peso";
+        $data['page_name'] = "Peso";
         $this->views->getView($this, "peso", $data);
     }
+
+    public function detalle($id) {
+        echo "Detalle del peso con ID: " . $id;
+    }
 }
-?>
