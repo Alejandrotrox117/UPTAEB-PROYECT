7c835c2b5ac28331d9656f3725159c469b2c23b6
MariaPineda20 <mariapinedabarroeta@gmail.com>
2025-05-14 08:37:32 -0400

    detalle en js de compras

--- app/assets/js/functions_compras.js
+++ app/assets/js/functions_compras.js
@@ -697,7 +697,7 @@ function calcularTotalesGeneralesModal() {
       btnGuardarCompraModal.textContent = "Guardando...";
 
       try {
-        const response = await fetch(`${URL_BASE}/Compras/setCompra`, {
+        const response = await fetch(`compras/setCompra`, {
           method: "POST",
           body: formData,
         });
