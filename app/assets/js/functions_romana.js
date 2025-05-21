d695e1d6601488d76bfc8619b1d788a270d84de6
MariaPineda20 <mariapinedabarroeta@gmail.com>
2025-05-21 19:58:05 -0400

    romana

---
+++ app/assets/js/functions_romana.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $("#TablaProductos").DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: "romana/getRomanaData",
+      type: "GET",
+      dataSrc: "data",
+    },
+    columns: [
+      { data: "peso", title: "Peso", width: "10%" },
+      { data: "fecha", title: "Fecha y Hora", width: "20%" },
+      { data: "fecha_creacion", title: "Fecha y Hora de Consulta", width: "30%" },
+    ],
+
+    language: {
+      decimal: "",
+      emptyTable: "No hay información",
+      info: "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+      infoEmpty: "Mostrando 0 to 0 of 0 Entradas",
+      infoFiltered: "(Filtrado de _MAX_ total entradas)",
+      infoPostFix: "",
+      thousands: ",",
+      lengthMenu: "Mostrar _MENU_ Entradas",
+      loadingRecords: "Cargando...",
+      processing: "Procesando...",
+      search: "Buscar:",
+      zeroRecords: "Sin resultados encontrados",
+      paginate: {
+        first: "Primero",
+        last: "Último",
+        next: "Siguiente",
+        previous: "Anterior",
+      },
+    },
+    destroy: true,
+    responsive: true,
+    pageLength: 10,
+    order: [[0, "asc"]],
+  });
+
+
+
+});  
